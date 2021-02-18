c3de49bd31a9acd6c38a28e3326af1c4188b5cd5
kaiweinan <kaiweinan@yahoo.com>
2021-02-18 16:27:55 -0500

    commit tree 9bcffc5373a3ecfaab7ddb65bc07b1c2d15e50d7

--- lib/jokes/api.rb
+++ lib/jokes/api.rb
@@ -1,40 +1,22 @@
 class Jokes::Api 
 
-   attr_accessor :url
 
-   @@all= []
-
-   def initialize(url)
-      @url = "https://official-joke-api.appspot.com/jokes/ten"
-      self.save
-   end
-
-  def self.create_jokes(category)
-    req_url= "https://official-joke-api.appspot.com/jokes/#{category}/ten"
-     data = HTTParty.get(req_url)
-    data.each do |data|
-      
-      jokes_hash = {
-         id: data["id"],
-        type: data["type"],
-        setup: data["setup"],
-        punchline: data["punchline"]
-         }
-      jokes = Jokes::Category.new(jokes_hash)
-      
-      end
-      Jokes::Category.all
+ def self.create_jokes(category)
+   req_url= "https://official-joke-api.appspot.com/jokes/#{category}/ten"
+    data = HTTParty.get(req_url)
+   data.each do |data|
      
-      
-  end
-
-
- def self.all
-  @@all
- end
-
- def save
-  @@all << self
+     jokes_hash = {
+        id: data["id"],
+       type: data["type"],
+       setup: data["setup"],
+       punchline: data["punchline"]
+        }
+     jokes = Jokes::Category.new(jokes_hash)
+     
+     end
+     Jokes::Category.all
+     
  end
 
 end
